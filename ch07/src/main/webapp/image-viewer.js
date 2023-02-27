069474e6d11127dfab01bdf50b596cd891a6e33c
Oh-HyunYoung <gusdud2709@gmail.com>
2023-02-27 21:06:06 +0900

    imageViewer #finish

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,37 +1,59 @@
-imageViewer = {
-	init: function(){
-		for(var i = 0; i<100; i++){
-			this._changeImage();
-		}
-	},
-	_changeImage: function(){
-		var index = Math.floor(Math.random()*this._images.length);
-		console.log(index);
-	},
-	_intevalId: null,
-	_images: [{
-		name:'국화', 
-		file:'Chrysanthemum.jpg'
-	},{
-		name:'사막', 
-		file:'Desert.jpg'
-	},{
-		name:'수국', 
-		file:'Hydrangeas.jpg'
-	},{
-		name:'해파리', 
-		file:'Jellyfish.jpg'
-	},{
-		name:'코알라', 
-		file:'Koala.jpg'
-	},{
-		name:'등대', 
-		file:'Lighthouse.jpg'
-	},{
-		name:'펭귄', 
-		file:'Penguins.jpg'
-	},{
-		name:'튤립', 
-		file:'Tulips.jpg'
-	}]
-};
+$(function() {
+	$("#btn-change").click(function() {
+		var index = Math.floor(Math.random() * imageViewer._images.length);
+		$("img").attr("src", imageViewer._images[index].file);
+	});
+
+	$("#btn-slideshow").click(function slideshow() {
+		var count = setInterval(function(){
+		var index = Math.floor(Math.random() * imageViewer._images.length);
+		$("img").attr("src", imageViewer._images[index].file);
+		},1000);
+		
+		$("#btn-slideshow").replaceWith("<button id='btn-slideshowstop'>슬라이드쇼 중지</button>");
+		$("#btn-slideshowstop").click(function(){
+			clearInterval(count);
+		})	
+	});
+
+	imageViewer = {
+		init: function() {
+			for (var i = 0; i < 100; i++) {
+				this._changeImage();
+			}
+		},
+
+		_changeImage: function() {
+			var index = Math.floor(Math.random() * this._images.length);
+			console.log(index);
+		},
+
+		_intevalId: null,
+		_images: [{
+			name: '국화',
+			file: 'images/Chrysanthemum.jpg'
+		}, {
+			name: '사막',
+			file: 'images/Desert.jpg'
+		}, {
+			name: '수국',
+			file: 'images/Hydrangeas.jpg'
+		}, {
+			name: '해파리',
+			file: 'images/Jellyfish.jpg'
+		}, {
+			name: '코알라',
+			file: 'images/Koala.jpg'
+		}, {
+			name: '등대',
+			file: 'images/Lighthouse.jpg'
+		}, {
+			name: '펭귄',
+			file: 'images/Penguins.jpg'
+		}, {
+			name: '튤립',
+			file: 'images/Tulips.jpg'
+		}]
+
+	}
+});
